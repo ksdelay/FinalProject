23a1509dbb5f026826fad1fd0fde6e9933e18275
Kevin S Delay <ksdelay@silk3.uvm.edu>
2015-12-07 22:21:16 -0500

    form stuff

--- addUser.php
+++ addUser.php
@@ -1,4 +1,36 @@
 <?php
+/* the purpose of this page is to display a form to allow a person to register
+ * the form will be sticky meaning if there is a mistake the data previously 
+ * entered will be displayed again. Once a form is submitted (to this same page)
+ * we first sanitize our data by replacing html codes with the html character.
+ * then we check to see if the data is valid. if data is valid enter the data 
+ * into the table and we send and dispplay a confirmation email message. 
+ * 
+ * if the data is incorrect we flag the errors.
+ * 
+ * Written By: Robert Erickson robert.erickson@uvm.edu
+ * Last updated on: November 10, 2015
+ * 
+ * 
+  -- --------------------------------------------------------
+  --
+  -- Table structure for table `tblRegister`
+  --
+
+  CREATE TABLE IF NOT EXISTS `tblRegister` (
+  `pmkRegisterId` int(11) NOT NULL AUTO_INCREMENT,
+  `fldEmail` varchar(65) DEFAULT NULL,
+  `fldDateJoined` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `fldConfirmed` tinyint(1) NOT NULL DEFAULT '0',
+  `fldApproved` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`pmkRegisterId`)
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ * I am using a surrogate key for demonstration, 
+ * email would make a good primary key as well which would prevent someone
+ * from entering an email address in more than one record.
+ */
+
 include "top.php";
 //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
 //
@@ -6,34 +38,35 @@ include "top.php";
 //
 // SECTION: 1a.
 // variables for the classroom purposes to help find errors.
-$debug = false;
+$debug = true;
 if (isset($_GET["debug"])) { // ONLY do this in a classroom environment
     $debug = true;
 }
 if ($debug)
     print "<p>DEBUG MODE IS ON</p>";
+
+
 //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
 //
 // SECTION: 1b Security
 //
 // define security variable to be used in SECTION 2a.
 $yourURL = $domain . $phpSelf;
+
 //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
 //
 // SECTION: 1c form variables
 //
 // Initialize variables one for each form element
 // in the order they appear on the form
-$name = "";
-$email = "youremail@email.com";
-$date = "";
+$email = "youremail@uvm.edu";
+
 //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
 //
 // SECTION: 1d form error flags
 //
 // Initialize Error Flags one for each form element we validate
 // in the order they appear in section 1c.
-$nameERROR = false;
 $emailERROR = false;
 
 //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
@@ -42,55 +75,47 @@ $emailERROR = false;
 //
 // create array to hold error messages filled (if any) in 2d displayed in 3c.
 $errorMsg = array();
-// array used to hold form values that will be written to a CSV file
-$dataRecord = array();
-$mailed=false; // have we mailed the information to the user?
+
+// used for building email message to be sent and displayed
+$mailed = false;
+$messageA = "";
+$messageB = "";
+$messageC = "";
+
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 //
 // SECTION: 2 Process for when the form is submitted
 //
 if (isset($_POST["btnSubmit"])) {
-    $date = new DateTime();
-echo $date->format('U = Y-m-d H:i:s') . "\n";
-
-$date->setTimestamp(1171502725);
-echo $date->format('U = Y-m-d H:i:s') . "\n";
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    //
-    // SECTION: 2a Security
-    // 
-    if (!securityCheck(true)) {
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//
+// SECTION: 2a Security
+//
+   if (!securityCheck(false)) {
         $msg = "<p>Sorry you cannot access this page. ";
-        $msg.= "Security breach detected and reported</p>";
-        die($msg);
+       $msg.= "Security breach detected and reported</p>";
+       die($msg);
     }
-    
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    //
-    // SECTION: 2b Sanitize (clean) data 
-    // remove any potential JavaScript or html code from users input on the
-    // form. Note it is best to follow the same order as declared in section 1c.
-    $name = htmlentities($_POST["txtname"], ENT_QUOTES, "UTF-8");
-    $dataRecord[] = $name;
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//
+// SECTION: 2b Sanitize (clean) data
+// remove any potential JavaScript or html code from users input on the
+// form. Note it is best to follow the same order as declared in section 1c.
     $email = filter_var($_POST["txtEmail"], FILTER_SANITIZE_EMAIL);
-    $dataRecord[] = $email;
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    //
-    // SECTION: 2c Validation
-    //
-    // Validation section. Check each value for possible errors, empty or
-    // not what we expect. You will need an IF block for each element you will
-    // check (see above section 1c and 1d). The if blocks should also be in the
-    // order that the elements appear on your form so that the error messages
-    // will be in the order they appear. errorMsg will be displayed on the form
-    // see section 3b. The error flag ($emailERROR) will be used in section 3c.
-    if ($name == "") {
-        $errorMsg[] = "Please enter your first name";
-        $nameERROR = true;
-    } elseif (!verifyAlphaNum($name)) {
-        $errorMsg[] = "Your first name appears to have extra character.";
-        $nameERROR = true;
-    }
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//
+// SECTION: 2c Validation
+//
+// Validation section. Check each value for possible errors, empty or
+// not what we expect. You will need an IF block for each element you will
+// check (see above section 1c and 1d). The if blocks should also be in the
+// order that the elements appear on your form so that the error messages
+// will be in the order they appear. errorMsg will be displayed on the form
+// see section 3b. The error flag ($emailERROR) will be used in section 3c.
+
+
     if ($email == "") {
         $errorMsg[] = "Please enter your email address";
         $emailERROR = true;
@@ -98,85 +123,116 @@ echo $date->format('U = Y-m-d H:i:s') . "\n";
         $errorMsg[] = "Your email address appears to be incorrect.";
         $emailERROR = true;
     }
-    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    //
-    // SECTION: 2d Process Form - Passed Validation
-    //
-    $query = 'INSERT INTO tblUsers SET pmkTime =?, pmkEmail =?,fldDate =? fldName = ?,   ';
-        $data = $dataRecord[$email];
-    // Process for when the form passes validation (the errorMsg array is empty)
-    //
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//
+// SECTION: 2d Process Form - Passed Validation
+//
+// Process for when the form passes validation (the errorMsg array is empty)
+//
     if (!$errorMsg) {
         if ($debug)
             print "<p>Form is valid</p>";
+
         //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         //
         // SECTION: 2e Save Data
         //
-        // This block saves the data to a CSV file.
-        $fileExt = ".csv";
-        $myFileName = "data/registration";
-        $filename = $myFileName . $fileExt;
-        if ($debug)
-            print "\n\n<p>filename is " . $filename;
-        // now we just open the file for append
-        $file = fopen($filename, 'a');
-        // write the forms informations
-        fputcsv($file, $dataRecord);
-        // close the file
-        fclose($file);
-        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        //
-        // SECTION: 2f Create message
-        //
-        // build a message to display on the screen in section 3a and to mail
-        // to the person filling out the form (section 2g).
-        $message = '<h2>Your information.</h2>';
         
-        foreach ($_POST as $key => $value) {
-            $message .= "<p>";
-            $camelCase = preg_split('/(?=[A-Z])/', substr($key, 3));
-            foreach ($camelCase as $one) {
-                $message .= $one . " ";
+        $primaryKey = "";
+        $dataEntered = false;
+        try {
+            $thisDatabase->db->beginTransaction();
+            $query = 'INSERT INTO tblUsers SET pmkTime=?, pmkEmail = ?';
+            $data = array($email, "hi");
+            if ($debug) {
+                print "<p>sql " . $query;
+                print"<p><pre>";
+                print_r($data);
+                print"</pre></p>";
             }
-            $message .= " = " . htmlentities($value, ENT_QUOTES, "UTF-8") . "</p>";
+            $results = $thisDatabase->insert($query, $data);
+
+           // $primaryKey = $thisDatabase->lastInsert();
+            //if ($debug)
+            //    print "<p>pmk= " . $primaryKey;
+
+// all sql statements are done so lets commit to our changes
+            $dataEntered = $thisDatabase->db->commit();
+            $dataEntered = true;
+            if ($debug)
+                print "<p>transaction complete ";
+        } catch (PDOException $e) {
+            $thisDatabase->db->rollback();
+            if ($debug)
+                print "Error!: " . $e->getMessage() . "</br>";
+            $errorMsg[] = "There was a problem with accpeting your data please contact us directly.";
         }
-        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        //
-        // SECTION: 2g Mail to user
-        //
-        // Process for mailing a message which contains the forms data
-        // the message was built in section 2f.
-        $to = $email; // the person who filled out the form
-        $cc = "";
-        $bcc = "";
-        $from = "WRONG site <noreply@yoursite.com>";
-        // subject of mail should make sense to your form
-        $todaysDate = strftime("%x");
-        $subject = "Research Study: " . $todaysDate;
-        $mailed = sendMail($to, $cc, $bcc, $from, $subject, $message);
-        
+        // If the transaction was successful, give success message
+        if ($dataEntered) {
+            if ($debug)
+                print "<p>data entered now prepare keys ";
+            //#################################################################
+            // create a key value for confirmation
+
+            $query = "SELECT fldDateJoined FROM tblRegister WHERE pmkRegisterId=" . $primaryKey;
+            $results = $thisDatabase->select($query);
+
+            $dateSubmitted = $results[0]["fldDateJoined"];
+
+            $key1 = sha1($dateSubmitted);
+            $key2 = $primaryKey;
+
+            if ($debug)
+                print "<p>key 1: " . $key1;
+            if ($debug)
+                print "<p>key 2: " . $key2;
+
+
+            //#################################################################
+            //
+            //Put forms information into a variable to print on the screen
+            //
+
+            $messageA = '<h2>Thank you for registering.</h2>';
+
+            $messageB = "<p>Click this link to confirm your registration: ";
+            $messageB .= '<a href="' . $domain . $path_parts["dirname"] . '/confirmation.php?q=' . $key1 . '&amp;w=' . $key2 . '">Confirm Registration</a></p>';
+            $messageB .= "<p>or copy and paste this url into a web browser: ";
+            $messageB .= $domain . $path_parts["dirname"] . '/confirmation.php?q=' . $key1 . '&amp;w=' . $key2 . "</p>";
+
+            $messageC .= "<p><b>Email Address:</b><i>   " . $email . "</i></p>";
+
+            //##############################################################
+            //
+            // email the form's information
+            //
+            $to = $email; // the person who filled out the form
+            $cc = "";
+            $bcc = "";
+            $from = "WRONG site <noreply@yoursite.com>";
+            $subject = "CS 148 registration that i forgot to change text";
+
+            $mailed = sendMail($to, $cc, $bcc, $from, $subject, $messageA . $messageB . $messageC);
+        } //data entered  
     } // end form is valid
-    
 } // ends if form was submitted.
 //#############################################################################
 //
 // SECTION 3 Display Form
 //
 ?>
-
 <article id="main">
-
     <?php
-    //####################################
-    //
-    // SECTION 3a.
-    //
-    // 
-    // 
-    // 
-    // If its the first time coming to the form or there are errors we are going
-    // to display the form.
+//####################################
+//
+// SECTION 3a.
+//
+//
+//
+//
+// If its the first time coming to the form or there are errors we are going
+// to display the form.
     if (isset($_POST["btnSubmit"]) AND empty($errorMsg)) { // closing of if marked with: end body submit
         print "<h1>Your Request has ";
         if (!$mailed) {
@@ -190,13 +246,13 @@ echo $date->format('U = Y-m-d H:i:s') . "\n";
         print "been sent</p>";
         print "<p>To: " . $email . "</p>";
         print "<p>Mail Message:</p>";
-        print $message;
+        print $messageA . $messageC;
     } else {
-        //####################################
-        //
-        // SECTION 3b Error Messages
-        //
-        // display any error messages before we print out the form
+//####################################
+//
+// SECTION 3b Error Messages
+//
+// display any error messages before we print out the form
         if ($errorMsg) {
             print '<div id="errors">';
             print "<ol>\n";
@@ -206,10 +262,10 @@ echo $date->format('U = Y-m-d H:i:s') . "\n";
             print "</ol>\n";
             print '</div>';
         }
-        //####################################
-        //
-        // SECTION 3c html Form
-        //
+//####################################
+//
+// SECTION 3c html Form
+//
         /* Display the HTML form. note that the action is to this same page. $phpSelf
           is defined in top.php
           NOTE the line:
@@ -222,56 +278,45 @@ echo $date->format('U = Y-m-d H:i:s') . "\n";
           make it stand out that a mistake happened here.
          */
         ?>
-
         <form action="<?php print $phpSelf; ?>"
               method="post"
               id="frmRegister">
-
             <fieldset class="wrapper">
                 <legend>Register Today</legend>
-                <p>You information will greatly help us with our research.</p>
-
+                <p>You will find Peace ...</p>
                 <fieldset class="wrapperTwo">
                     <legend>Please complete the following form</legend>
-
                     <fieldset class="contact">
                         <legend>Contact Information</legend>
-                        <label for="txtName" class="required">First Name
-                            <input type="text" id="txtName" name="txtName"
-                                   value="<?php print $name; ?>"
-                                   tabindex="100" maxlength="45" placeholder="Enter your name"
-                                   <?php if ($nameERROR) print 'class="mistake"'; ?>
-                                   onfocus="this.select()"
-                                   autofocus>
-                        </label>
-                        
+
                         <label for="txtEmail" class="required">Email
                             <input type="text" id="txtEmail" name="txtEmail"
                                    value="<?php print $email; ?>"
                                    tabindex="120" maxlength="45" placeholder="Enter a valid email address"
                                    <?php if ($emailERROR) print 'class="mistake"'; ?>
-                                   onfocus="this.select()" 
+                                   onfocus="this.select()"
                                    >
                         </label>
                     </fieldset> <!-- ends contact -->
-                    
                 </fieldset> <!-- ends wrapper Two -->
-                
                 <fieldset class="buttons">
                     <legend></legend>
                     <input type="submit" id="btnSubmit" name="btnSubmit" value="Register" tabindex="900" class="button">
                 </fieldset> <!-- ends buttons -->
-                
             </fieldset> <!-- Ends Wrapper -->
         </form>
-
-    <?php
+        <?php
     } // end body submit
     ?>
-
 </article>
 
-<?php include "footer.php"; ?>
 
+
+<?php
+include "footer.php";
+if ($debug)
+    print "<p>END OF PROCESSING</p>";
+?>
+</article>
 </body>
 </html>
